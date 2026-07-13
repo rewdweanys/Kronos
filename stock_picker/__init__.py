b5ed4702381084048fc8b96f2e9305797c83ff0a
rewdweanys <drew.sweany8@gmail.com>
2026-07-12 21:05:21 -0400

    Export backtesting helpers

--- stock_picker/__init__.py
+++ stock_picker/__init__.py
@@ -1,5 +1,6 @@
 """Research-first stock ranking tools built around the Kronos model."""
 
+from .backtest import BacktestObservation, summarize_backtest, walk_forward_backtest
 from .forecasts import Forecast, KronosForecaster, TrendBaselineForecaster
 from .scoring import Recommendation, aggregate_forecasts
 
@@ -9,4 +10,7 @@ __all__ = [
     "TrendBaselineForecaster",
     "Recommendation",
     "aggregate_forecasts",
+    "BacktestObservation",
+    "walk_forward_backtest",
+    "summarize_backtest",
 ]
